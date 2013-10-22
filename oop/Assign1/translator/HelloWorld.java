d4e4f5f67a7d5fed55e7e2e7961ef569f4b6cb62
Ankit Goel <ankitgoel@172-26-115-98.dynapool.nyu.edu>
2013-10-22 18:52:03 -0400

    hello world compiles now

--- oop/Assign1/translator/HelloWorld.java
+++ oop/Assign1/translator/HelloWorld.java
@@ -1,9 +1,8 @@
-public Class HelloWorld{
+public class HelloWorld{
 
- 
     public static void main(String[] args) {
        
-       System.out.println("Hello World!")
+       System.out.println("Hello World!");
        int x = 0;
        x = 4 + 5;
        
