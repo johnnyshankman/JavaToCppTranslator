32178a1897d0481f9f4326e1276137e1adb0138c
Tara Wilson <tara.wilson@nyu.edu>
2013-10-23 19:28:24 -0400

    fixed inheritance constructor

---
+++ oop/Assign1/translator/Testtwo.java
@@ -0,0 +1,18 @@
+class Testtwo{
+
+ 
+	public class Square{
+
+		public Square(int x){
+			
+		}
+	}
+
+    public static void main(String[] args) {
+       
+       String st = "What's up?";
+       Square z = new Square(3);
+       
+    }
+    
+}
