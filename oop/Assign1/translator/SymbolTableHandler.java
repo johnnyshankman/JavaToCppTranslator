87e5c2cdd314bca2d62188617f3daca5b301e740
John L. Shankman <johnlshankman@Johns-MacBook-Pro.local>
2013-11-13 23:55:43 -0500

    added comments

--- oop/Assign1/translator/SymbolTableHandler.java
+++ oop/Assign1/translator/SymbolTableHandler.java
@@ -4,18 +4,40 @@ import xtc.util.SymbolTable;
 import xtc.util.Runtime;
 
 import xtc.tree.GNode;
-import xtc.tree.Node;
 
+
+/**
+ * 
+ * @author johnlshankman
+ *
+ * In essence, this an exact copy of the JavaExternalAnalyzer
+ * This subclass adds the Block and ForStatement visitors
+ * because JavaExternalAnalyzer does not currently visit
+ * Blocks or For statements (even though they need scoping).
+ * 
+ * Furthermore, JavaExternalAnalyzer has been slightly modified.
+ * The visitMethodDeclaration, visitClassDeclaration, and visitCompilationUnit
+ * methods have all been modified so that they utilize mark(n);
+ * 
+ * visitMethodDeclaration has an added visit(n); call in order
+ * to ensure we visit the block/body of every method. This explains
+ * why we added the visitBlock and visitForStatment methods below.
+ * 
+ * TODO: visitPackageDeclaration in JavaExternalAnalyzer needs to 
+ * utilze mark(n); but whenever I attempt to add the line, the 
+ * program throws a NullPointerException right when I invoke mark(n); 
+ * on the packageDeclaration GNode. No clue why.
+ */
 
 public class SymbolTableHandler extends xtc.lang.JavaExternalAnalyzer {
 	
-	//run dispatch on an instance of JavaExternalAnalyzer to create the symbol table
+	//call to supeclass's constructor
 	public SymbolTableHandler(Runtime runtime, SymbolTable table)
 	{
 		super(runtime, table);
 	}
 	
-	//added in order to visit method bodies
+	//added in order to find local variables and scoping information within blocks
 	public void visitBlock(GNode n) 
 	{
 		_table.enter(_table.freshName("block"));
@@ -24,17 +46,16 @@ public class SymbolTableHandler extends xtc.lang.JavaExternalAnalyzer {
 		_table.exit();
 	}
 	
+	//added in order to scope the iterator within For loops
 	public void visitForStatement(GNode n) 
 	{
 		_table.enter(_table.freshName("forStatement"));
 		_table.mark(n);
 		visit(n);
 		_table.exit();
-	}
-	
-	public void visit(Node n) 
-	{
-		for (Object o : n) if (o instanceof Node) dispatch((Node) o);
-	}
-		
+	}	
 }
+
+
+
+
